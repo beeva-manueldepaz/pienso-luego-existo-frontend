80d416a3e6b3bf100f5d26701a37510317d56e65
César <cesar.alonso@beeva.com>
2016-01-28 10:28:16 +0100

    Adding controller and template

--- app/controllers/add.js
+++ app/controllers/add.js
@@ -9,6 +9,11 @@ export default Ember.Controller.extend({
         console.log(this.get('precio'));
         console.log(this.get('descriṕcion'));
         console.log(this.get('fCaducidad'));
+        
+        var response = document.createElement('h4');
+        var textNode = document.createTextNode(`Has dado de alta el pienso ${this.marca} correctamente`);
+        response.appendChild(textNode);
+        this.set('response', response);
       }
     }
 });
