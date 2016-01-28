e4ab844b97f07a2d668cdde0d7f2f7472dd39a94
beeva-joseGutierrez <jose.gutierrez@beeva.com>
2016-01-28 16:34:38 +0100

    starting adapters

---
+++ app/adapters/pienso.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.RESTAdapter.extend({
+
+ host: 'http://localhost:3000'
+ });
+
+
+//Could not find module `pienso/adapters/application` imported from `pienso/adapters/pienso`
--- app/routes/show.js
+++ app/routes/show.js
@@ -1,21 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    this.store.push({
-      data: [{
-        id: 1,
-        type: 'pienso',
-        attributes: {
-          marca: "Purina",
-          modelo: "Junior Pro",
-          peso: 2.5,
-          precio: 5.99,
-          descripcion: "Indicado para perros jovenes",
-          fCaducidad: "1453912360",
-          logo: "sdfasdfasf"
-        }
-      }]
-    })
+
+  model: function() {
+    //return Ember.$.getJSON('http://localhost:3000/piensos/');
+    return this.store.findRecord('pienso', 1).then(function(success) {
+      console.log(success)
+    });
   }
 });
