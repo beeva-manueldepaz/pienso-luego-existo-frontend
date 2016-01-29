4632eb9c21c1aa21be8ac0842b4ceadffeba8a0f
beeva-joseGutierrez <jose.gutierrez@beeva.com>
2016-01-29 11:02:51 +0100

    view show-all created

--- app/routes/show.js
+++ app/routes/show.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
-    return this.store.findRecord('pienso', params.id).then(function(success) {
+    return this.store.findRecord('pienso', params.queryParams).then(function(success) {
     	return success;
     });
   }
